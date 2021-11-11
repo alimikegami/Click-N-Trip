50e1ca34ba6cc23c430dcb1691f39990e6a5419e
Renrensan <pasha.renaisan@gmail.com>
2021-11-11 13:00:07 +0800

    Split navbar file and refined css

--- resources/views/LoginPage.blade.php
+++ resources/views/LoginPage.blade.php
@@ -9,52 +9,43 @@
     <title>Login</title>
 </head>
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark" id='custom-navbar'>
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Click-n-Trip</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">View Deals</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Profile
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Login</a></li>
-                  <li><a class="dropdown-item" href="#">Sign-Up</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <!-- Login Page Starts Here -->
       <div class="parent container d-flex justify-content-center align-items-center h-100" id='login-container'>
-        <form class="child" action="/authenticate" method="POST">
-            @csrf
-            <h2>Welcome to Click-n-Trip!</h2>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" name='email' id="email" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <div class="form-container container-fluid">
+          <div class="row">
+            <div class="col">
+              <div class="container">
+                <form class="child" action="/authenticate" method="POST">
+                    @csrf
+                    <h2 class='pt-5' id='caption'>Log-In</h2>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" name='email' id="email" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="password">
+                    </div>
+                    <div class="d-grid gap-2 pb-3">
+                      <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                    <div class='pb-5'>
+                      <p href="#" class='text-center text-black'>Forgot Password</p>
+                    </div>
+                    
+            </form>
+          </div>
+          </div>
+          <div class="col">
+            <div class="container" id="login-right-content">
+              <h2 class='text-center'id='right-text' >Dont have an account?</h2>
+              <div class="d-grid gap-2 pb-5">
+                <button type="submit" class="btn btn-primary">Sign-Up</button>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" id="password">
-            </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-        <p class="mt-2">Don't have an account?</p>
-        <button type="submit" class="btn btn-primary">Sign-Up</button>
-    </form>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
--- resources/views/RegisterPage.blade.php
+++ resources/views/RegisterPage.blade.php
@@ -6,63 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/LoginPage.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <title>Register</title>
 </head>
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark" id='custom-navbar'>
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Click-n-Trip</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">View Deals</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Profile
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Login</a></li>
-                  <li><a class="dropdown-item" href="#">Sign-Up</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <!-- Register Page Starts Here -->
       <div class="parent container d-flex justify-content-center align-items-center h-100" id='login-container'>
-        <form class="child" action="/store" method="post">
-            @csrf
-            <h2>Welcome to Click-n-Trip!</h2>
-            <div class="mb-3">
-                <label for="name" class="form-label">Full Name</label>
-                <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp">
+        <div class='form-container container-fluid'>
+          <div class="row">
+            <div class="col">
+              <div class="container">
+                <form action="/store" method="post" class='register-form'>
+                    @csrf
+                    <h2 class='pt-5' id='caption'>Sign-Up</h2>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Full Name</label>
+                        <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password-confirm" class="form-label">Confirm Password</label>
+                        <input type="password" name='password-confirm' class="form-control" id="password-confirm">
+                    </div>
+                    <div class="d-grid gap-2 pb-5">
+                      <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
+                    </div>
+              </form>
             </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="col">
+            <div class="container" id="right-content">
+              <h2 class='text-center'id='right-text' >Already have an account?</h2>
+              <div class="d-grid gap-2 pb-5">
+                <button type="submit" class="btn btn-primary">Login</button>
+              </div>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" id="password">
-            </div>
-            <div class="mb-3">
-                <label for="password-confirm" class="form-label">Confirm Password</label>
-                <input type="password" name='password-confirm' class="form-control" id="password-confirm">
-            </div>
-        <button type="submit" class="btn btn-primary">Sign Up</button>
-        <p class="mt-2">Don't have an account?</p>
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+          </div>
+        </div>
+      </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
