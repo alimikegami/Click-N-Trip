4167374e446cffbed33e03696d7dc4667fb30bf2
Alim Ikegami <alimikegami1@gmail.com>
2021-11-15 14:27:13 +0800

    refactor: refactor views

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,57 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/LoginPage.css">
-    <link rel="stylesheet" href="../css/index.css">
-    <title>Login</title>
-</head>
-<body>
-      <!-- Login Page Starts Here -->
-      <div class="parent container d-flex justify-content-center align-items-center h-100" id='login-container'>
-        <div class="form-container container-fluid rounded mt-5 mb-5">
-          <div class="row">
-            <div class="col-lg-2 rounded" id='container-image'>
-            </div>
-            <div class="col">
-              <div class="container">
-                <form class="child" action="/users/login" method="POST">
-                    @csrf
-                    <h2 class='pt-5' id='caption'>Log-In</h2>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" name='email' id="email" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control @error('email') is-invalid @enderror" id="password">
-                    </div>
-                        @error('email')
-                            <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                    <div class="d-grid gap-2 pb-3">
-                      <button type="submit" class="btn btn-primary">Login</button>
-                    </div>
-                    <div class='pb-5'>
-                      <p href="#" class='text-center text-black'>Forgot Password</p>
-                    </div>   
-                </form>
+@extends('layouts.main-layout')
+
+@section('css')
+  <link rel="stylesheet" href="../css/LoginPage.css">
+@endsection
+
+@section('body')
+<div class="parent container d-flex justify-content-center align-items-center h-100" id='login-container'>
+  <div class="form-container container-fluid rounded mt-5 mb-5">
+    <div class="row">
+      <div class="col-lg-2 rounded" id='container-image'>
+      </div>
+      <div class="col">
+        <div class="container">
+          <form class="child" action="/users/login" method="POST">
+              @csrf
+              <h2 class='pt-5' id='caption'>Log-In</h2>
+              <div class="mb-3">
+                  <label for="email" class="form-label">Email address</label>
+                  <input type="email" class="form-control @error('email') is-invalid @enderror" name='email' id="email" aria-describedby="emailHelp">
               </div>
-          </div>
-          <div class="col d-flex justify-content-center align-items-center">
-            <div class="container" id="login-right-content">
-              <h2 class='text-center'id='right-text' >Dont have an account?</h2>
-              <div class="d-grid gap-2 pb-5">
-                <button type="submit" class="btn btn-primary">Sign-Up</button>
-            </div>
-          </div>
+              <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control @error('email') is-invalid @enderror" id="password">
+              </div>
+                  @error('email')
+                      <div class="alert alert-danger">{{ $message }}</div>
+                  @enderror
+              <div class="d-grid gap-2 pb-3">
+                <button type="submit" class="btn btn-primary">Login</button>
+              </div>
+              <div class='pb-5'>
+                <p href="#" class='text-center text-black'>Forgot Password</p>
+              </div>   
+          </form>
         </div>
+    </div>
+    <div class="col d-flex justify-content-center align-items-center">
+      <div class="container" id="login-right-content">
+        <h2 class='text-center'id='right-text' >Dont have an account?</h2>
+        <div class="d-grid gap-2 pb-5">
+          <button type="submit" class="btn btn-primary">Sign-Up</button>
       </div>
     </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+  </div>
+</div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+@endsection
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,64 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/LoginPage.css">
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <title>Register</title>
-</head>
-<body>
-      <!-- Register Page Starts Here -->
-      <div class="parent container d-flex justify-content-center align-items-center h-100">
-        <div class='form-container container-fluid rounded mt-5 mb-5'>
-          <div class="row">
-            <div class="col-lg-2 rounded" id='container-image'>
-              
-            </div>
-            <div class="col">
-              <div class="container">
-                <form action="/users/register" method="post" class='register-form'>
-                    @csrf
-                    <h2 class='pt-5' id='caption'>Sign-Up</h2>
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Full Name</label>
-                        <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
-                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password-confirm" class="form-label">Confirm Password</label>
-                        <input type="password" name='password-confirm' class="form-control" id="password-confirm">
-                    </div>
-                    <div class="d-grid gap-2 pb-5">
-                      <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
-                    </div>
-              </form>
-            </div>
-          </div>
-          <div class="col d-flex justify-content-center align-items-center">
-            <div class="container" id="right-content">
-              <h2 class='text-center'id='right-text' >Already have an account?</h2>
-              <div class="d-grid gap-2 pb-5">
-                <button type="submit" class="btn btn-primary">Login</button>
+@extends('layouts.main-layout')
+
+@section('css')
+  <link rel="stylesheet" href="../css/LoginPage.css">
+@endsection
+<!-- Register Page Starts Here -->
+
+@section('body')
+<div class="parent container d-flex justify-content-center align-items-center h-100">
+  <div class='form-container container-fluid rounded mt-5 mb-5'>
+    <div class="row">
+      <div class="col-lg-2 rounded" id='container-image'>
+        
+      </div>
+      <div class="col">
+        <div class="container">
+          <form action="/users/register" method="post" class='register-form'>
+              @csrf
+              <h2 class='pt-5' id='caption'>Sign-Up</h2>
+              <div class="mb-3">
+                  <label for="name" class="form-label">Full Name</label>
+                  <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp">
               </div>
-            </div>
-          </div>
+              <div class="mb-3">
+                  <label for="email" class="form-label">Email address</label>
+                  <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control" id="password">
+              </div>
+              <div class="mb-3">
+                  <label for="password-confirm" class="form-label">Confirm Password</label>
+                  <input type="password" name='password-confirm' class="form-control" id="password-confirm">
+              </div>
+              <div class="d-grid gap-2 pb-5">
+                <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
+              </div>
+        </form>
+      </div>
+    </div>
+    <div class="col d-flex justify-content-center align-items-center">
+      <div class="container" id="right-content">
+        <h2 class='text-center'id='right-text' >Already have an account?</h2>
+        <div class="d-grid gap-2 pb-5">
+          <button type="submit" class="btn btn-primary">Login</button>
         </div>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+  </div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+@endsection
